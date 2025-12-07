8f777e07433ebbc9389ea9f344355d8267859f86
VixenHavoc1 <vixenhavoc1@gmail.com>
2025-12-07 20:28:47 +0530

    Add type annotation for movie in map function

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ return (
 <main className="p-6">
 <h1 className="text-3xl font-bold mb-4">Latest Movies</h1>
 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-{movies.map((movie) => (
+{movies.map((movie: any) => (
 <MovieCard key={movie.id} movie={movie} />
 ))}
 </div>
